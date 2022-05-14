8d9b467bb43f5f59687f3c0c1d060d70ce553f0b
ChristianNikodemus <christian.nikodemus@gmail.com>
2022-05-13 21:32:28 -0400

    Made navbar responsive

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import logo from "./img/logo_v1.png";
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import {
+  Container,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 class NavBar extends React.Component {
   render() {
     return (
       <>
-        <Navbar fixed="top" bg="light">
-          <Container>
+        <Navbar fixed="top" bg="light" expand="lg">
+          <Container fluid>
             <Navbar.Brand href="#home">
               <img
                 src={logo}
@@ -17,12 +25,19 @@ class NavBar extends React.Component {
                 alt="Christian Nikodemus logo"
               />
             </Navbar.Brand>
-            <Nav className="me-2">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#work">Work</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0 justify-content-end flex-grow-1 pe-3"
+                style={{ maxHeight: "200px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#work">Work</Nav.Link>
+                <Nav.Link href="#contact">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
       </>
