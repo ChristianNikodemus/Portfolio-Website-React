151e3f596a889d574a3d60fe56db85589e3c285d
ChristianNikodemus <christian.nikodemus@gmail.com>
2022-05-15 16:21:48 -0400

    Added work section

--- src/Work.jsx
+++ src/Work.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import {
-  Card,
-  ListGroup,
-  ListGroupItem,
-  Container,
-  Row,
-  Col,
-} from "react-bootstrap";
+import simpleIndexingApp from "./img/Simple-indexing-app-img.png";
+import myFilmsClient from "./img/myFilms-client-img.png";
+import meet from "./img/Meet-img.png";
+import chatterBox from "./img/Chatter_Box-img.png";
+import myFilmsAngular from "./img/myFilms-Angular-client.png";
+import { Card, ListGroup, Button, Container, Row, Col } from "react-bootstrap";
 
 class Work extends React.Component {
   render() {
@@ -14,76 +12,234 @@ class Work extends React.Component {
       <div>
         <h1>Work</h1>
         <Container>
-          <Row xl={5} lg={4} md={3} sm={2} xs={1} className="g-4">
+          <Row lg={3} md={2} sm={1} xs={1} className="g-4">
             <Col>
               <Card>
+                <Card.Header as="h2">Simple Indexing App</Card.Header>
                 <Card.Img
                   variant="top"
-                  src="holder.js/100px180?text=Image cap"
+                  src={simpleIndexingApp}
+                  alt="Simple indexing web application user interface"
                 />
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    A small web application with HTML, CSS, and JavaScript that
+                    loads data from an external API and enables the viewing of
+                    data points in detail.
                   </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                  <ListGroupItem>Cras justo odio</ListGroupItem>
-                  <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                  <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-                  <Card.Link href="#">Card Link</Card.Link>
-                  <Card.Link href="#">Another Link</Card.Link>
+                  <Button
+                    variant="outline-primary"
+                    href="https://christiannikodemus.github.io/Simple-Indexing-App/"
+                    target="_blank"
+                  >
+                    Open Project
+                  </Button>
+                  <Button
+                    variant="outline-primary"
+                    href="https://github.com/ChristianNikodemus/Simple-Indexing-App"
+                    target="_blank"
+                  >
+                    See project on Github
+                  </Button>
+                  <ListGroup>
+                    <ListGroup.Item>HTML</ListGroup.Item>
+                    <ListGroup.Item>CSS</ListGroup.Item>
+                    <ListGroup.Item>JavaScript</ListGroup.Item>
+                    <ListGroup.Item>Bootstrap</ListGroup.Item>
+                    <ListGroup.Item>jQuery</ListGroup.Item>
+                    <ListGroup.Item>
+                      Application Programming Interface (API)
+                    </ListGroup.Item>
+                  </ListGroup>
                 </Card.Body>
               </Card>
             </Col>
             <Col>
               <Card>
-                <Card.Img
-                  variant="top"
-                  src="holder.js/100px180?text=Image cap"
-                />
+                <Card.Header as="h2">myFilms Database</Card.Header>
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    A server-side component of a “movies” web application. The
+                    web application provides users with access to information
+                    about different movies, directors, and genres. Users are
+                    able to sign up, update their personal information, and
+                    create a list of their favorite movies.
                   </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                  <ListGroupItem>Cras justo odio</ListGroupItem>
-                  <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                  <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-                  <Card.Link href="#">Card Link</Card.Link>
-                  <Card.Link href="#">Another Link</Card.Link>
+
+                  <Button
+                    variant="outline-primary"
+                    href="https://github.com/ChristianNikodemus/myFilmsDB"
+                    target="_blank"
+                  >
+                    See project on Github
+                  </Button>
+                  <ListGroup>
+                    <ListGroup.Item>Node.js</ListGroup.Item>
+                    <ListGroup.Item>Express</ListGroup.Item>
+                    <ListGroup.Item>MongoDB</ListGroup.Item>
+                    <ListGroup.Item>Postman</ListGroup.Item>
+                    <ListGroup.Item>Heroku</ListGroup.Item>
+                  </ListGroup>
                 </Card.Body>
               </Card>
             </Col>
             <Col>
               <Card>
+                <Card.Header as="h2">myFilms React Front-end</Card.Header>
                 <Card.Img
                   variant="top"
-                  src="holder.js/100px180?text=Image cap"
+                  src={myFilmsClient}
+                  alt="React front end user interface"
                 />
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    Using React, I built the client-side for an application
+                    called myFilms based on its existing server-side code (REST
+                    API and database).
                   </Card.Text>
+                  <Button
+                    variant="outline-primary"
+                    href="https://my-films-client-nrdjnfybp-christiannikodemus.vercel.app/"
+                    target="_blank"
+                  >
+                    Open Project
+                  </Button>
+                  <Button
+                    variant="outline-primary"
+                    href="https://github.com/ChristianNikodemus/myFilms-client"
+                    target="_blank"
+                  >
+                    See project on Github
+                  </Button>
+                  <ListGroup>
+                    <ListGroup.Item>HTML</ListGroup.Item>
+                    <ListGroup.Item>CSS</ListGroup.Item>
+                    <ListGroup.Item>JavaScript</ListGroup.Item>
+                    <ListGroup.Item>React</ListGroup.Item>
+                    <ListGroup.Item>React Bootstrap</ListGroup.Item>
+                    <ListGroup.Item>React Redux</ListGroup.Item>
+                  </ListGroup>
                 </Card.Body>
-                <ListGroup className="list-group-flush">
-                  <ListGroupItem>Cras justo odio</ListGroupItem>
-                  <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                  <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                </ListGroup>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Header as="h2">Meet App</Card.Header>
+                <Card.Img
+                  variant="top"
+                  src={meet}
+                  alt="React front end user interface"
+                />
                 <Card.Body>
-                  <Card.Link href="#">Card Link</Card.Link>
-                  <Card.Link href="#">Another Link</Card.Link>
+                  <Card.Text>
+                    A serverless, progressive web application (PWA) with React
+                    using a test-driven development (TDD) technique. The
+                    application uses the Google Calendar API to fetch upcoming
+                    events.
+                  </Card.Text>
+                  <Button
+                    variant="outline-primary"
+                    href="https://christiannikodemus.github.io/meet/"
+                    target="_blank"
+                  >
+                    Open Project
+                  </Button>
+                  <Button
+                    variant="outline-primary"
+                    href="https://github.com/ChristianNikodemus/meet"
+                    target="_blank"
+                  >
+                    See project on Github
+                  </Button>
+                  <ListGroup>
+                    <ListGroup.Item>React</ListGroup.Item>
+                    <ListGroup.Item>
+                      Google Calendar API and OAuth2 authentication
+                    </ListGroup.Item>
+                    <ListGroup.Item>AWS lambda</ListGroup.Item>
+                    <ListGroup.Item>React axios and async/await</ListGroup.Item>
+                    <ListGroup.Item>
+                      Recharts (data visualization)
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Header as="h2">Chatter Box</Card.Header>
+                <Card.Img
+                  variant="top"
+                  src={chatterBox}
+                  alt="React native mobile user interface"
+                  style={{
+                    maxWidth: "9em",
+                    margin: "0 auto",
+                    marginTop: "0.5em",
+                  }}
+                />
+                <Card.Body>
+                  <Card.Text>
+                    A chat app for mobile devices using React Native. The app
+                    provides users with a chat interface and options to share
+                    images and their location.
+                  </Card.Text>
+                  <Button
+                    variant="outline-primary"
+                    href="https://github.com/ChristianNikodemus/Chatter-Box"
+                    target="_blank"
+                  >
+                    See project on Github
+                  </Button>
+                  <ListGroup>
+                    <ListGroup.Item>React Native</ListGroup.Item>
+                    <ListGroup.Item>Expo</ListGroup.Item>
+                    <ListGroup.Item>Google Firestore Database</ListGroup.Item>
+                    <ListGroup.Item>Gifted Chat Library</ListGroup.Item>
+                  </ListGroup>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Header as="h2">myFilms Angular Front-end</Card.Header>
+                <Card.Img
+                  variant="top"
+                  src={myFilmsAngular}
+                  alt="Angular front end user interface"
+                />
+                <Card.Body>
+                  <Card.Text>
+                    Using Angular, I built the client-side for an application
+                    called myFilms based on its existing server-side code (REST
+                    API and database), with supporting documentation.
+                  </Card.Text>
+                  <Button
+                    variant="outline-primary"
+                    href="https://christiannikodemus.github.io/myFilms-Angular-client/welcome"
+                    target="_blank"
+                  >
+                    Open Project
+                  </Button>
+                  <Button
+                    variant="outline-primary"
+                    href="https://github.com/ChristianNikodemus/myFilms-Angular-client"
+                    target="_blank"
+                  >
+                    See project on Github
+                  </Button>
+                  <ListGroup>
+                    <ListGroup.Item>Angular</ListGroup.Item>
+                    <ListGroup.Item>Angular Material</ListGroup.Item>
+                    <ListGroup.Item>TypeScript</ListGroup.Item>
+                    <ListGroup.Item>TypeDoc (documentation)</ListGroup.Item>
+                  </ListGroup>
+                  <Card.Link
+                    href="https://trello.com/b/ttMAyK4v/myfilms-angular-client"
+                    target="_blank"
+                  >
+                    Kanban board
+                  </Card.Link>
                 </Card.Body>
               </Card>
             </Col>
