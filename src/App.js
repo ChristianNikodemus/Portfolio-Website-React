e96f60ca1e7b2d74f98e078752affdcfbb97cce8
ChristianNikodemus <christian.nikodemus@gmail.com>
2022-06-04 12:26:34 -0400

    Created layout for navigation bar scroll feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // import logo from "./logo.svg";
 import bgImage from "./img/DSC00280_16x9.jpg";
 import "./App.css";
+
+// Components
 import NavBar from "./NavBar.jsx";
 import Home from "./Home.jsx";
 import About from "./About.jsx";
@@ -16,11 +18,11 @@ function App() {
         className="App-main"
         style={{ backgroundImage: `url(${bgImage})`, backgroundSize: "cover" }}
       >
-        <Home id="#home" />
+        <Home id="homeContainer" />
       </div>
-      <About id="#about" />
-      <Work id="#work" />
-      <Contact />
+      <About id="aboutContainer" />
+      <Work id="workContainer" />
+      <Contact id="contactContainer" />
       <FooterNav />
     </div>
   );
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./img/logo_v1.png";
 import { Container, Navbar, Nav } from "react-bootstrap";
+
+const navLinks = [
+  { navLinkId: "Home", scrollToId: "homeContainer" },
+  { navLinkId: "About", scrollToId: "aboutContainer" },
+  { navLinkId: "Work", scrollToId: "workContainer" },
+  { navLinkId: "Contact", scrollToId: "contactContainer" },
+];
+
+const NavLink = ({
+  navLinkId,
+  scrollToId,
+  activeNavLinkId,
+  setActiveNavLinkId,
+}) => {
+  const handleClick = () => {
+    setActiveNavLinkId(navLinkId);
+    document.getElementById(scrollToId).scrollIntoView({
+      behavior: "smooth", // gives an ease-in-out effect to scroll
+    });
+  };
+
+  return (
+    <span
+      id={navLinkId}
+      className={activeNavLinkId === navLinkId ? "activeClass" : ""}
+      onClick={handleClick}
+    >
+      {navLinkId}
+    </span>
+  );
+};
+
+const Navigation = () => {
+  const [activeNavLinkId, setActiveNavLinkId] = useState("");
+
+  return (
+    <Nav
+      className="me-auto my-2 my-lg-0 justify-content-end flex-grow-1 pe-3"
+      style={{ maxHeight: "200px" }}
+      navbarScroll
+    >
+      {navLinks.map(({ navLinkId, scrollToId }) => (
+        <Nav.Link>
+          <NavLink
+            navLinkId={navLinkId}
+            scrollToId={scrollToId}
+            activeNavLinkId={activeNavLinkId}
+            setActiveNavLinkId={setActiveNavLinkId}
+          />
+        </Nav.Link>
+      ))}
+    </Nav>
+  );
+};
 
 class NavBar extends React.Component {
   render() {
     return (
       <>
         <Navbar fixed="top" bg="light" expand="lg">
-          <Container fluid>
+          <Container fluid style={{ maxWidth: "1600px" }}>
             <Navbar.Brand href="#home">
               <img
                 src={logo}
@@ -19,16 +73,7 @@ class NavBar extends React.Component {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0 justify-content-end flex-grow-1 pe-3"
-                style={{ maxHeight: "200px" }}
-                navbarScroll
-              >
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#about">About</Nav.Link>
-                <Nav.Link href="#work">Work</Nav.Link>
-                <Nav.Link href="#contact">Contact</Nav.Link>
-              </Nav>
+              <Navigation />
             </Navbar.Collapse>
           </Container>
         </Navbar>
